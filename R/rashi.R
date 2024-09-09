3deee0c114ec1a7cc2ae11b865dfd88cc062123e
Ajay Singh <37625112+Ajay-singh1@users.noreply.github.com>
2024-09-09 20:02:07 +0530

    Fixed wrong calculation method for rashi function

--- R/rashi.R
+++ R/rashi.R
@@ -12,8 +12,8 @@
 #' rashi(gregorian_to_jd(30,8,2022))
 rashi <- function(jd){
   swephR::swe_set_sid_mode(swephR::SE$SIDM_LAHIRI,0,0)
-  s = moon_longitude(jd)
-  lunar_nirayana = (moon_longitude(jd) - swephR::swe_get_ayanamsa_ex_ut(jd,swephR::SE$FLG_SWIEPH + swephR::SE$FLG_NONUT)$daya) %% 360
-  return (ceiling(lunar_nirayana / 30))
+  s = sun_longitude(jd)
+  solar_nirayana = (sun_longitude(jd) - swephR::swe_get_ayanamsa_ex_ut(jd,swephR::SE$FLG_SWIEPH + swephR::SE$FLG_NONUT)$daya) %% 360
+  return (ceiling(solar_nirayana / 30))
 }
 # ---------------------------------------------------------------------------- #
